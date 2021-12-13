6d194110d57d0737cd5391a580ba4bb163403065
bichvan278 <mintmint27899@gmail.com>
2021-12-13 17:28:09 +0700

    code signin

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -34,13 +34,7 @@ router.get('/users', async (req, res) => {
 router.get('/user/:id', async (req, res) => {
     const id = req.params.id
     try {
-        const findid = {
-            selector : {
-                _id: id
-            },
-            fields: [ "fullname", "email", "phone", "email", "username", "password" ]
-        }
-        const userid = await db.find(findid)
+        const userid = await db.get(id)
         if(!userid) {
             res.status(404).send()
         }
@@ -69,35 +63,39 @@ router.post('/register', async (req, res) => {
 });
 
 // Sign In
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
     try {
-        if(username === 'lanpui' && password === 'lanpuibanhwet') {
-            res.status(200).send('Admin is login succesful!')
-        }else if(username === 'lienlien' && password === 'lien123'){
-            res.status(200).send('Librarian is login succesful!')
-        }else if(username === 'henry' && password === 'henry123'){
-            res.status(200).send('Reader is login succesful!')
-        }else {
-            res.send('ACC is not available!')
+        // if(username === 'lanpui' && password === 'lanpuibanhwet') {
+        //     res.status(200).send('Admin is login succesful!')
+        // }else if(username === 'lienlien' && password === 'lien123'){
+        //     res.status(200).send('Librarian is login succesful!')
+        // }else if(username === 'henry' && password === 'henry123'){
+        //     res.status(200).send('Reader is login succesful!')
+        // }else {
+        //     res.send('ACC is not available!')
+        // }
+
+        const find_user = {
+            selector : {
+                username: username,
+                password: password
+            },
+            fields: [ "_id", "fullname", "email", "phone", "email", "username", "password", "role" ]
+        }
+        const userid = await db.find(find_user)
+        // console.log(get)
+        if(username === username && password === password) {
+            res.status(200).send(userid)
+        }
+        else{
+            res.status(500).send("Try again!")
         }
     }catch (e) {
         console.log(e)
     }
-
-    // const queryUser = {
-    //     username,
-    //     password
-    // }
-
-    // await couch.get(dbname, dbviewURL, queryUser).then((data, headers, status) => {
-    //         res.send(data)
-    //         console.log('Login successful!')
-    // }, (err) => {
-    //     res.send(err)
-    // })
 });
 
 // Read all doc in users
